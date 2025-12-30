48d32f38cbfbfce06323853ab7f04f612c4f473a
cimexlabs-web <127245719+Akilaudantha@users.noreply.github.com>
2025-12-31 01:41:57 +0530

    edited

--- pharmacy-backend/main.py
+++ pharmacy-backend/main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
-from database import engine, Base
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
 from models import User
 
 app = FastAPI()
 
-# Create tables in MySQL (if not exist)
-Base.metadata.create_all(bind=engine)
-
-@app.get("/")
-def home():
-    return {"message": "Hello, Pharmacy System!"}
+@app.post("/login")
+def login(
+    email: str, 
+    password: str, 
+    db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user.password != password:
+        raise HTTPException(status_code=400, detail="Incorrect password")
+    return {
+        "message": f"Login successful! Welcome {user.name}", 
+        "role": user.role
+    }
